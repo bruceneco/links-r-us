461996a82059a1eaff1716d79df6b746ea614858
Bruce Neco <bruce.neco@gmail.com>
2024-05-06 20:45:08 -0300

    feat: add iterator port helper

---
+++ internal/ports/iterator.go
@@ -0,0 +1,11 @@
+package ports
+
+// Iterator is a contract to pass along a set of items orchestrating its progress.
+type Iterator interface {
+	// Next advances the iterator. If no more items are available or an error occurs, calls to Next() return false.
+	Next() bool
+	// Error returns the last error encountered by the Iterator.
+	Error() error
+	// Close releases any resources associated with an Iterator.
+	Close() error
+}
