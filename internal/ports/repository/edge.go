e43508582abbea6c33738abd795c262663b59e5c
Bruce Neco <bruce.neco@gmail.com>
2024-06-06 21:19:54 -0300

    feat: add Edges method to edge repository port

--- internal/ports/repository/edge.go
+++ internal/ports/repository/edge.go
@@ -1,7 +1,13 @@
 package ports
 
-import "github.com/bruceneco/links-r-us/internal/application/core/domain"
+import (
+	"github.com/bruceneco/links-r-us/internal/application/core/domain"
+	"github.com/bruceneco/links-r-us/internal/ports"
+	"github.com/google/uuid"
+	"time"
+)
 
 type EdgeRepository interface {
 	Upsert(edge *domain.Edge) error
+	Edges(fromID, toID uuid.UUID, updatedBefore time.Time) (ports.EdgeIterator, error)
 }
