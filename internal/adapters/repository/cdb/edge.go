a8b8435a50f6a0646c6faf23dd9e5dde2c08cdaa
Bruce Neco <bruce.neco@gmail.com>
2024-06-06 21:20:23 -0300

    feat: implement Edges method from edge repository on cdb

--- internal/adapters/repository/cdb/edge.go
+++ internal/adapters/repository/cdb/edge.go
@@ -7,7 +7,9 @@ import (
 	"github.com/bruceneco/links-r-us/internal/application/core/domain"
 	"github.com/bruceneco/links-r-us/internal/ports"
 	repoPorts "github.com/bruceneco/links-r-us/internal/ports/repository"
+	"github.com/google/uuid"
 	"github.com/lib/pq"
+	"time"
 )
 
 type EdgeRepository struct {
@@ -26,8 +28,8 @@ var upsertEdgeQuery = `
 	RETURNING id, updated_at
 `
 
-func (l *EdgeRepository) Upsert(edge *domain.Edge) error {
-	row := l.db.QueryRow(upsertEdgeQuery, edge.Src, edge.Dst)
+func (r *EdgeRepository) Upsert(edge *domain.Edge) error {
+	row := r.db.QueryRow(upsertEdgeQuery, edge.Src, edge.Dst)
 	if err := row.Scan(&edge.ID, &edge.UpdatedAt); err != nil {
 		if isForeignKeyViolationError(err) {
 			err = ports.GraphErrUnknownEdgeLinks
@@ -45,4 +47,21 @@ func isForeignKeyViolationError(err error) bool {
 		return false
 	}
 	return pqErr.Code.Name() == "foreign_key_violation"
+}
+
+var edgesInPartitionQuery = `
+	SELECT id, src, dst, updated_at 
+	FROM edges 
+	WHERE 
+	    src >= $1 AND 
+	    src < $2 AND 
+	    updated_at < $3
+`
+
+func (r *EdgeRepository) Edges(fromID, toID uuid.UUID, updatedBefore time.Time) (ports.EdgeIterator, error) {
+	rows, err := r.db.Query(edgesInPartitionQuery, fromID, toID, updatedBefore)
+	if err != nil {
+		return nil, fmt.Errorf("edges: %w", err)
+	}
+	return newEdgeIterator(rows), nil
 }
