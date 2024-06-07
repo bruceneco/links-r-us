bd292b304fe42aaaf5e0e2c023dc68927f9860b9
Bruce Neco <bruce.neco@gmail.com>
2024-06-06 21:20:03 -0300

    feat: add Links method to link repository port

--- internal/ports/repository/link.go
+++ internal/ports/repository/link.go
@@ -2,10 +2,13 @@ package ports
 
 import (
 	"github.com/bruceneco/links-r-us/internal/application/core/domain"
+	"github.com/bruceneco/links-r-us/internal/ports"
 	"github.com/google/uuid"
+	"time"
 )
 
 type LinkRepository interface {
 	Upsert(link *domain.Link) error
 	Find(id uuid.UUID) (*domain.Link, error)
+	Links(fromId, toId uuid.UUID, accessedBefore time.Time) (ports.LinkIterator, error)
 }
